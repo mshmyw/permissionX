7612ffac4d3b2cb24d4685ef85347a66e957f7c8
摘新 <shuanghua.msh@alibaba-inc.com>
2021-10-13 23:12:29 +0800

    feat(): permissionx test

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.permissionx.mshmyw.PermissionX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.makeCallBtn).setOnClickListener {
+            PermissionX.request(this,
+            Manifest.permission.CALL_PHONE) {
+                allGranted, deniedList ->
+                if(allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this,"You denied $deniedList", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try{
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
